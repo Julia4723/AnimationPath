0740cccd1b00e12e492d0f33ef489d78ddd6bac0
My <uk472363@yanex.ru>
2024-05-27 14:44:01 +0300

    Commit 2

--- AnimationPath/AnimationView.swift
+++ AnimationPath/AnimationView.swift
@@ -13,40 +13,34 @@ struct AnimationView: View {
     @State private var heartColor = false
     
     var body: some View {
-        HStack {
-            ZStack {
-                
-                Circle()
-                    .frame(width: 120, height: 120)
-                    .foregroundColor(circleColor ? .white : .red)
-                    .offset(y: 267)
-                
-                Image(systemName: "heart.fill")
-                    .resizable()
-                    .foregroundColor(circleColor ? .red : .white)
-                    .scaleEffect(CGSize(width: 0.18, height: 0.09))
-                    .offset(y: 270)
-                    .onTapGesture {
-                        withAnimation(.spring(response: 0.3,  blendDuration: 0.3)) {
-                            self.circleColor.toggle()
-                            
-                            // circleColor.toggle()
-                            awardIsShowing.toggle()
-                        }
+        
+        ZStack {
+            Circle()
+                .frame(width: 120, height: 120)
+                .foregroundColor(circleColor ? .white : .red)
+                .offset(y: 267)
+            
+            Image(systemName: "heart.fill")
+                .resizable()
+                .foregroundColor(circleColor ? .red : .white)
+                .scaleEffect(CGSize(width: 0.18, height: 0.09))
+                .offset(y: 270)
+                .onTapGesture {
+                    withAnimation(.spring(response: 0.3,  blendDuration: 0.3)) {
+                        self.circleColor.toggle()
+                        awardIsShowing.toggle()
                     }
-                
-                
-                if awardIsShowing {
-                    
-                    PathView()
-                        .frame(width: 400, height: 400)
-                        .animation(.easeOut, value: awardIsShowing)
                 }
-                
+            
+            
+            if awardIsShowing {
+                PathView()
+                    .frame(width: 400, height: 400)
+                    .animation(.easeOut, value: awardIsShowing)
             }
         }
-        
     }
+    
     
     
     private func buttonAction() {
--- AnimationPath/PathView.swift
+++ AnimationPath/PathView.swift
@@ -9,10 +9,7 @@ import SwiftUI
 
 struct PathView: View {
     var body: some View {
-        
-        GeometryReader { geometry in
             ZStack {
-                
                 GeometryReader { geometry in
                     let width = geometry.size.width
                     let hight = geometry.size.height
@@ -26,7 +23,7 @@ struct PathView: View {
                             LinearGradient (
                                 gradient: Gradient(colors: [.pink, .blue]),
                                 startPoint: UnitPoint(x: 2, y: 0),
-                                endPoint: UnitPoint(x: 2, y: 1 )
+                                endPoint: UnitPoint(x: 2, y: 1)
                             )
                             .opacity(0.5)
                         )
@@ -37,7 +34,7 @@ struct PathView: View {
                         .foregroundColor(.yellow)
                         .scaleEffect(CGSize(width: 1, height: 0.6))
                         .scaleEffect(0.7)
-                        .offset(x: size / 5.2, y: size / 4.8)
+                        .offset(x: size / 5.8, y: size / 4.5)
                         .opacity(0.7)
                         .padding()
                     
@@ -71,7 +68,6 @@ struct PathView: View {
                         path.addLine(to: CGPoint(x: farLine, y: farLine))
                         path.addLine(to: CGPoint(x: nearLine, y: farLine))
                         path.addLine(to: CGPoint(x: middle - nearLine, y: nearLine))
-                        
                     }
                     .fill(Gradient(colors: [.white, .blue, .black]))
                     .scaleEffect(0.7)
@@ -84,19 +80,16 @@ struct PathView: View {
                         path.addLine(to: CGPoint(x: farLine, y: farLine))
                         path.addLine(to: CGPoint(x: nearLine, y: farLine))
                         path.addLine(to: CGPoint(x: middle - nearLine, y: nearLine))
-                        
                     }
-                    .fill(Gradient(colors: [.white, .blue , .black]))
+                    .fill(Gradient(colors: [.white, .blue, .black]))
                     .scaleEffect(0.4)
-                    .offset(x: -size / 3.1, y: size / 5.0)
+                    .offset(x: -size / 3.2, y: size / 5.0)
                     .padding()
-                    
                 }
             }
             .padding()
         }
     }
-}
 
 struct PathView_Previews: PreviewProvider {
     static var previews: some View {
